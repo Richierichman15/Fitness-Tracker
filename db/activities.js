d0128b33671232bcc7896ebf9725b9ee0d54edff
Gary <garyarneys@gmail.com>
2023-03-27 09:43:34 -0500

    Still working on updateActivity function. Getting close to completing it.

--- db/activities.js
+++ db/activities.js
@@ -35,12 +35,10 @@ async function getAllActivities() {
 // async function getActivityById(id) {}
 const getActivityById = async(id) => {
   try{
-    console.log('IDDDDDDDD',id);
     const {rows: [getId]} = await client.query(`
     SELECT * FROM activities
     WHERE id=${id};
     `,[])
-console.log('getIDDDDDDDDD',getId);
 return getId;
   } catch(err) {
     console.log(err);
@@ -64,11 +62,57 @@ const getActivityByName = async(name) => {
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {}
 
-async function updateActivity({ id, ...fields }) {
+// async function updateActivity({ id, ...fields }) {
+
+const updateActivity = async({ id, ...fields }) => {
+
+
+console.log('The id before all is:',id,'The fields before is:',fields);
+//ID is 12, FIELDS is {name: 'softball}
+ try {
+
+  if (!fields.name){
+    console.log('id before name',id,'no name.............',fields.name,'desc:',fields.description);
+    //id is 13, name is undefined, desc is Football for life
+    const {rows: [newAct2]} = await client.query(`
+    UPDATE activities
+    SET name=$1
+    WHERE id=$2
+    RETURNING *;  
+  
+    `,[fields.name, id, ])
+    console.log('id after name:',id,'descript after....',fields.description,'name after:',fields.name);
+    //id is 13, desc is Football for life, name is undefined
+    return newAct2
+  }
+if (!fields.description){
+  console.log('id before desc:',id,'no desc...............',fields.description,'name?',fields.name);
+  //id is 12, desc is undefined, name is softball
+  const {rows: [newAct]} = await client.query(`
+  UPDATE activities
+  SET description=$1
+  WHERE id=$2
+  RETURNING *;  
+
+  `,[fields.description, id])
+  console.log('id after desc:',id,'name after:',fields.name,'descript after....',fields.description);
+  //id is 12, name is softball, desc is undefined
+  return newAct;
+}
+
+  console.log('iddddd:',id,'fieldsssssss:', fields);
+ 
+
+  
+ } catch(err) {
+  console.log(err);
+ }
+
+}
   // don't try to update the id
   // do update the name and description
   // return the updated activity
-}
+
 
 module.exports = {
   getAllActivities,
