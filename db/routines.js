89e9b6dd15ee09bbbc572f34ed43bb32fa60a69d
Gitonga <richierichman15@gmail.com>
2023-03-29 10:02:13 -0500

    added try catch

--- db/routines.js
+++ db/routines.js
@@ -2,24 +2,31 @@ const client = require("./client");
 
 const createRoutine = async({ creatorId, isPublic, name, goal }) =>{
 
-
+  try {
   const {rows: [result]} = await client.query(`
     INSERT INTO routines("creatorId", "isPublic", name, goal)
     VALUES($1, $2, $3, $4)
     RETURNING *;
   `, [ creatorId, isPublic, name, goal])
+  
   console.log('ROWS', [result]);
   return creatorId, isPublic, name, goal
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 const getRoutineById = async(id) => {
-  
+  try {
     const {rows: [getId]} = await client.query(`
     SELECT * FROM routines(id)
     WHERE id=${id}
    `, [getId])
     console.log('here', getId);
     return getId
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 const getRoutinesWithoutActivities = async() => {
@@ -35,7 +42,14 @@ const getAllRoutines = async() => {
 }
 
 const getAllPublicRoutines = async() => {
-
+  try {
+  const rows = await client.query(`
+   SELECT 
+  `)
+  return rows
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 const getAllRoutinesByUser = async({ username }) => {
