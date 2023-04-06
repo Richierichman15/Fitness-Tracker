109d6ce9710b72458f25433c99e7b0c807a090f2
Gary <garyarneys@gmail.com>
2023-04-06 16:41:43 -0500

    Got the API route up with some guidance. Tests are still hanging though.

--- api/activities.js
+++ api/activities.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const { getAllActivities } = require('../db/activities');
+
 // GET /api/activities/:activityId/routines
+
 // GET /api/activities
-router.get('/api/activities', async (req, res, next) => {
-    try {
-        const activitesGotten = await getAllActivities()
-        console.log('activitiesGottennnnnnnn:', activitesGotten)
-        res.send({ activitesGotten })
-    } catch(err) {
-        next(err);
-    }
-});
+
 // POST /api/activities
+
 // PATCH /api/activities/:activityId
-module.exports = {
-    router
-}
+
+module.exports = router;
 
 
+
+
+// my code below, may be too soon to implement
 
 // const express = require('express');
 // const router = express.Router();
-
+// const { getAllActivities } = require('../db/activities');
 // // GET /api/activities/:activityId/routines
-
 // // GET /api/activities
-
+// router.get('/api/activities', async (req, res, next) => {
+//     try {
+//         const activitesGotten = await getAllActivities()
+//         console.log('activitiesGottennnnnnnn:', activitesGotten)
+//         res.send({ activitesGotten })
+//     } catch(err) {
+//         next(err);
+//     }
+// });
 // // POST /api/activities
-
 // // PATCH /api/activities/:activityId
-
-// module.exports = router;
+// module.exports = {
+//     router
+// }
--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const morgan = require('morgan');
+const apiRouter = require('./api');
+// const cors = require('cors');
+
 
 // Setup your Middleware and API Router here
+
+// const path = require('path');
+
+app.use(morgan('dev'));
+
+const client = require('./db/client');
+
+client.connect();
+
+app.use('/api', apiRouter)
+
+// app.use('/', (req, res));
 
 module.exports = app;
