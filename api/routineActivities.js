b064bcc15a1540f3bbd471e1393bd54360ed6c8d
Gary <garyarneys@gmail.com>
2023-04-14 04:52:27 -0500

    Completed all tests for routineActivities API.

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { getRoutineActivityById, updateRoutineActivity } = require('../db/routine_activities');
+const {
+    getRoutineActivityById,
+    updateRoutineActivity,
+    destroyRoutineActivity
+     } = require('../db/routine_activities');
 const { getRoutineById } = require('../db/routines');
+const { getActivityById } = require('../db/activities');
+
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const SECRET_KEY = process.env.JWT_SECRET;
@@ -48,12 +54,51 @@ router.patch('/:routineActivityId', async (req, res) => {
 
 });
 
-
-
-
-
-
-
 // DELETE /api/routine_activities/:routineActivityId
+
+router.delete('/:routineActivityId', async (req, res) => {
+    const header = req.headers.authorization
+
+    
+    try{
+
+        const { routineActivityId } = req.params
+        const routineActivity = await getRoutineActivityById(routineActivityId)
+        
+        const { routineId, activityId, count, duration } = routineActivity;
+        const routine = await getRoutineById(routineId)
+        const activity = await getActivityById(activityId)
+        const { id } = activity
+        if (header) {
+
+            const token = req.headers.authorization.split(' ')[1];
+            const verified = jwt.verify(token, SECRET_KEY); 
+
+            if (verified.id === routine.creatorId) {
+                await destroyRoutineActivity(routineActivityId)
+                return res.send({ 
+                    activityId: id,
+                    count: count,
+                    duration: duration,
+                    id: id,
+                    routineId: routineId })
+            }
+
+            if (verified.id != routine.creatorId){
+                res.status(403).send({message: `User ${verified.username} is not allowed to delete ${routine.name}`,
+                error: "I am error",
+                name: 'unauthorizedUser'
+                })
+            }
+
+
+            }
+
+    } catch(err) {
+        console.log(err);
+    }
+
+
+})
 
 module.exports = router;
